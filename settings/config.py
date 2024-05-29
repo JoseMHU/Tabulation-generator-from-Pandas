3c2ea602817c728feb181f676d111ce0968e0f7e
José García Urbáez <garcia.urbaez.j@gmail.com>
2024-05-29 19:30:23 -0400

    💎style: Adjustment of default table styles.

--- settings/config.py
+++ settings/config.py
@@ -15,45 +15,45 @@ class JsonFile:
                     "Header": {
                         "Alignment": {"horizontal": 'left', "vertical": 'center'
                                       },
-                        "Font": {"name": "Gotham Book", "size": 12, "bold": True,
+                        "Font": {"name": "Calibri", "size": 12, "bold": True,
                                  "color": "45455d"
                                  },
-                        "Border": {"border_style": 'medium', "color": '86754d'
+                        "Border": {"border_style": 'medium', "color": '444444'
                                    }
                         },
                     "Sub_Header": {
                         "Alignment": {"horizontal": 'center', "vertical": 'center'
                                       },
-                        "Font": {"name": "Gotham Light", "size": 11, "bold": False,
+                        "Font": {"name": "Calibri Light", "size": 11, "bold": False,
                                  "color": "45455d"
                                  }
                         },
                     "Var_Header": {
                         "Alignment": {"horizontal": 'center', "vertical": 'center'
                                       },
-                        "Font": {"name": "Gotham Light", "size": 11, "bold": True,
+                        "Font": {"name": "Calibri Light", "size": 11, "bold": True,
                                  "color": "45455d"
                                  },
-                        "Border": {"border_style": 'medium', "color": '86754d'
+                        "Border": {"border_style": 'medium', "color": '444444'
                                    },
-                        "PatternFill": {"start_color": "c3b6a1",
-                                        "end_color": "c3b6a1",
+                        "PatternFill": {"start_color": "C5D9F1",
+                                        "end_color": "C5D9F1",
                                         "fill_type": "solid"}
                         },
                     "Body": {
-                            "Font": {"color": "45455d", "name": "Gotham Medium",
+                            "Font": {"color": "45455d", "name": "Calibri",
                                      "size": 10},
                             "Alignment": {"horizontal": 'center',
                                           "vertical": 'center'}
                             },
                     "Footer": {
                         "Font": {"color": "45455d",
-                                 "name": "Gotham Extra Light",
+                                 "name": "Calibri",
                                  "size": 8},
                         "Alignment": {"horizontal": 'left',
                                       "vertical": 'center'},
                         "Border": {"border_style": 'medium',
-                                   "color": '86754d'}
+                                   "color": '444444'}
                         }
                     }
                 with open(self.__path, "w") as file:
